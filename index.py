cfdef6275ea7a594184bc8281fc11b3ba07307a8
Matias Carbini <matias.carbini@gmail.com>
2022-10-07 18:51:49 -0300

    commit inicial

---
+++ index.py
@@ -0,0 +1,59 @@
+from PIL import Image
+
+"""
+  CONVERTIR WEBP --> PNG
+"""
+
+im = Image.open('image/webp/auricular-gamer-1.webp').convert('RGB')
+im.save('image/png/auricular-gamer-1.png','png')
+
+im = Image.open('image/webp/auricular-gamer-2.webp').convert('RGB')
+im.save('image/png/auricular-gamer-2.png','png')
+
+im = Image.open('image/webp/auricular1.webp').convert('RGB')
+im.save('image/png/auricular1.png','png')
+
+im = Image.open('image/webp/auricular2.webp').convert('RGB')
+im.save('image/png/auricular2.png','png')
+
+im = Image.open('image/webp/cooler1.webp').convert('RGB')
+im.save('image/png/cooler1.png','png')
+
+im = Image.open('image/webp/cooler2.webp').convert('RGB')
+im.save('image/png/cooler2.png','png')
+
+im = Image.open('image/webp/disco1.webp').convert('RGB')
+im.save('image/png/disco1.png','png')
+
+im = Image.open('image/webp/disco2.webp').convert('RGB')
+im.save('image/png/disco2.png','png')
+
+im = Image.open('image/webp/i5.webp').convert('RGB')
+im.save('image/png/i5.png','png')
+
+im = Image.open('image/webp/microsd.webp').convert('RGB')
+im.save('image/png/microsd.png','png')
+
+im = Image.open('image/webp/mouse-inal-1.webp').convert('RGB')
+im.save('image/png/mouse-inal-1.png','png')
+
+im = Image.open('image/webp/mouse-inal-2.webp').convert('RGB')
+im.save('image/png/mouse-inal-2.png','png')
+
+im = Image.open('image/webp/mouse1.webp').convert('RGB')
+im.save('image/png/mouse1.png','png')
+
+im = Image.open('image/webp/mouse2.webp').convert('RGB')
+im.save('image/png/mouse2.png','png')
+
+im = Image.open('image/webp/parlante1.webp').convert('RGB')
+im.save('image/png/parlante1.png','png')
+
+im = Image.open('image/webp/parlante2.webp').convert('RGB')
+im.save('image/png/parlante2.png','png')
+
+im = Image.open('image/webp/tablet1.webp').convert('RGB')
+im.save('image/png/tablet1.png','png')
+
+im = Image.open('image/webp/tablet2.webp').convert('RGB')
+im.save('image/png/tablet2.png','png')
